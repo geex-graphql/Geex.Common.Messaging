5a6fd7ddeb5a0d6b0f41e62cb19867f0920f700b
Lulus <snys98@outlook.com>
2022-03-08 18:54:05 +0800

    update 6.0

--- Api/Aggregates/Messages/IMessage.cs
+++ Api/Aggregates/Messages/IMessage.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 
 using Geex.Common.Abstraction;
+using Geex.Common.Abstraction.MultiTenant;
 
 using KuanFang.Rms.MessageManagement.Messages;
 
@@ -12,7 +13,7 @@ namespace Geex.Common.Messaging.Api.Aggregates.Messages
     /// <summary>
     /// this is a aggregate root of this module, we name it the same as the module feel free to change it to its real name
     /// </summary>
-    public interface IMessage : IEntity
+    public interface IMessage : IEntity, ITenantFilteredEntity
     {
         string? FromUserId { get; }
         public MessageType MessageType { get; }
--- Core/Aggregates/Messages/Message.cs
+++ Core/Aggregates/Messages/Message.cs
@@ -2,79 +2,78 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Geex.Common.Abstraction;
-using Microsoft.Extensions.DependencyInjection;
+using Geex.Common.Abstraction.MultiTenant;
+using Geex.Common.Abstraction.Storage;
 using Geex.Common.Messaging.Api.Aggregates.Messages;
 using KuanFang.Rms.MessageManagement.Messages;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using MongoDB.Entities;
-using Entity = Geex.Common.Abstraction.Storage.Entity;
 
-namespace Geex.Common.Messaging.Core.Aggregates.Messages
+namespace Geex.Common.Messaging.Core.Aggregates.Messages;
+
+/// <summary>
+///     普通message
+/// </summary>
+public class Message : Entity, IMessage
 {
-    /// <summary>
-    /// 普通message
-    /// </summary>
-    public class Message : Entity, IMessage
+    protected Message()
     {
-        private ILogger<Message> Logger => ServiceProvider.GetService<ILogger<Message>>();
-        protected Message()
-        {
-        }
-        public Message(string text, MessageSeverityType severity = MessageSeverityType.Info)
+    }
+
+    public Message(string text, MessageSeverityType severity = MessageSeverityType.Info)
         : this()
-        {
-            Title = text;
-            this.Severity = severity;
-            this.MessageType = MessageType.Notification;
-        }
-        public Message(string text, IMessageContent content = default, MessageSeverityType severity = MessageSeverityType.Info) : this(text, severity)
-        {
-            this.Content = content;
-            this.MessageType = content switch
-            {
-                ToDoContent => MessageType.Todo,
-                InteractContent => MessageType.Interact,
-                _ => MessageType.Notification
-            };
-        }
-        public virtual IQueryable<MessageDistribution> Distributions => DbContext.Queryable<MessageDistribution>().Where(x => x.MessageId == this.Id);
-        public string? FromUserId { get; private set; }
-        public IList<string> ToUserIds => this.Distributions.ToList().Select(x => x.ToUserId).ToList();
-        public string Title { get; set; }
-        public DateTimeOffset Time => CreatedOn;
+    {
+        Title = text;
+        Severity = severity;
+        MessageType = MessageType.Notification;
+    }
 
-        public MessageType MessageType { get; set; }
-        public IMessageContent Content { get; private set; }
-        public MessageSeverityType Severity { get; set; }
-        public async Task<Message> DistributeAsync(params string[] userIds)
+    public Message(string text, IMessageContent content = default,
+        MessageSeverityType severity = MessageSeverityType.Info) : this(text, severity)
+    {
+        Content = content;
+        MessageType = content switch
         {
-            if (this.Distributions.Any())
-            {
-                return this;
-            }
+            ToDoContent => MessageType.Todo,
+            InteractContent => MessageType.Interact,
+            _ => MessageType.Notification
+        };
+    }
 
-            var distributions = userIds.Select(x => new MessageDistribution(this.Id, x)).ToList();
-            DbContext.Attach(distributions);
+    public virtual IQueryable<MessageDistribution> Distributions =>
+        DbContext.Queryable<MessageDistribution>().Where(x => x.MessageId == Id);
 
-            return this;
-        }
+    private ILogger<Message> Logger => ServiceProvider.GetService<ILogger<Message>>();
+    public IMessageContent Content { get; private set;}
+    public string? FromUserId { get; private set; }
 
-        /// <summary>
-        /// 标记当前消息针对特定用户已读
-        /// </summary>
-        /// <param name="userId"></param>
-        public void MarkAsRead(string userId)
-        {
-            var userDistribution = this.Distributions.FirstOrDefault(x => x.ToUserId == userId);
-            if (userDistribution != default)
-            {
-                userDistribution.IsRead = true;
-            }
-            else
-            {
-                Logger.LogWarning("试图标记不存在的消息分配记录已读.");
-            }
-        }
+    public MessageType MessageType { get; set; }
+    public MessageSeverityType Severity { get; set; }
+    public DateTimeOffset Time => CreatedOn;
+    public string Title { get; set; }
+    public IList<string> ToUserIds => Distributions.ToList().Select(x => x.ToUserId).ToList();
+    public string? TenantCode { get; protected set; }
+
+    public async Task<Message> DistributeAsync(params string[] userIds)
+    {
+        if (Distributions.Any()) return this;
+
+        var distributions = userIds.Select(x => new MessageDistribution(Id, x)).ToList();
+        DbContext.Attach(distributions);
+
+        return this;
+    }
+
+    /// <summary>
+    ///     标记当前消息针对特定用户已读
+    /// </summary>
+    /// <param name="userId"></param>
+    public void MarkAsRead(string userId)
+    {
+        var userDistribution = Distributions.FirstOrDefault(x => x.ToUserId == userId);
+        if (userDistribution != default)
+            userDistribution.IsRead = true;
+        else
+            Logger.LogWarning("试图标记不存在的消息分配记录已读.");
     }
 }
